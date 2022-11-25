3528a559963f567e56317f371455775f60ff9920
fullstop125 <hassanmoses125@gmail.com>
2022-11-25 22:43:07 +0300

    fix required changes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+  after_save :like_counter
+
+  private
 
   def like_counter
     posts.increment!(:likes_counter)
