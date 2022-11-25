0e75426061d95bf67d1c4acaf69ae400ad1030eb
fullstop125 <hassanmoses125@gmail.com>
2022-11-25 23:04:47 +0300

    add validations and tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
   after_save :post_counter
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
